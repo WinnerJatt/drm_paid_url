624ae7135cc53106631a87c641c67e46c8ad9c4d
Lucky JAAT <180734617+WinnerJatt@users.noreply.github.com>
2024-12-31 19:52:27 +0530

    Update main.py for port

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import os
 from bot import Bot
 
 if __name__ == '__main__':
-    Bot().run()
+    # Get the port from the environment variable, default to 5000 if not set
+    port = int(os.environ.get('PORT', 5000))
+
+    # Initialize and run the bot
+    app = Bot()
+    app.run(host='0.0.0.0', port=port)
